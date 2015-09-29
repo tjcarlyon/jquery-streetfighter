dacaf471cff2c2b29d606e55901c563aa19f3b3e
Thomas Carlyon <tjcarlyon@gmail.com>
2015-09-28 21:45:18 -0500

    animation

---
+++ js/app.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
+    $(document).keydown(function(event) {
+    if(event.which == 88) {
+      $(".ryu-cool").show();
+      $(".ryu-throwing").hide();
+      $(".ryu-ready").hide();
+      $(".ryu-still").hide();
+      
+    }
+  })
+
+  $(document).keyup(function(event) {
+    if(event.which == 88) {
+      $(".ryu-still").show();
+      $(".ryu-ready").hide();
+      $(".ryu-throwing").hide();
+      $(".ryu-cool").hide();
+        }
+  });
+});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
+
+    
+
+    
